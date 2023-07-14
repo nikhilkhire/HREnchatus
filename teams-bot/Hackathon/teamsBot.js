4cc5b0411ee23eab0982aede7a48ba557dc10bea
Nikhil Khire <nikhilkhire@gmail.com>
2023-07-14 15:21:53 +0200

    Initial Commit

---
+++ teams-bot/Hackathon/teamsBot.js
@@ -0,0 +1,106 @@
+const { TeamsActivityHandler, CardFactory, TurnContext } = require("botbuilder");
+const rawWelcomeCard = require("./adaptiveCards/welcome.json");
+const rawLearnCard = require("./adaptiveCards/learn.json");
+const cardTools = require("@microsoft/adaptivecards-tools");
+const fetch = require('node-fetch');
+
+async function toText(reader) {
+  //const reader = body.getReader();
+  const decoder = new TextDecoder();
+  const chunks = [];
+
+  async function read() {
+    const { done, value } = await reader.read();
+
+    if (done) {
+      return chunks.join('');
+    }
+    const chunk = decoder.decode(value, { stream: true });
+    chunks.push(chunk);
+    return read();
+  }
+
+  return read();
+}
+
+class TeamsBot extends TeamsActivityHandler {
+  constructor() {
+    super();
+
+    // record the likeCount
+    this.likeCountObj = { likeCount: 0 };
+
+    this.onMessage(async (context, next) => {
+      console.log("Running with Message Activity.");
+      let txt = context.activity.text;
+      console.log(txt);
+      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+      if (removedMentionText) {
+        // Remove the line break
+        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      }
+
+      // Trigger command by IM text
+      switch (txt) {
+        case "welcome": {
+          const card = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
+          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
+          break;
+        }
+        case "learn": {
+          this.likeCountObj.likeCount = 0;
+          const card = cardTools.AdaptiveCards.declare(rawLearnCard).render(this.likeCountObj);
+          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
+          break;
+        }
+        default: {
+          const resp = await fetch("https://sibnick.men/api/", {method: 'POST', body: txt});
+          var body = await resp.text();
+          console.log(body);
+          body = body.substring(body.indexOf("\r\n\r\n") + 4);
+          await context.sendActivity(body);
+        }
+        /**
+         * case "yourCommand": {
+         *   await context.sendActivity(`Add your response here!`);
+         *   break;
+         * }
+         */
+      }
+
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    // Listen to MembersAdded event, view https://docs.microsoft.com/en-us/microsoftteams/platform/resources/bot-v3/bots-notifications for more events
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        if (membersAdded[cnt].id) {
+          const card = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
+          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
+          break;
+        }
+      }
+      await next();
+    });
+  }
+
+  // Invoked when an action is taken on an Adaptive Card. The Adaptive Card sends an event to the Bot and this
+  // method handles that event.
+  async onAdaptiveCardInvoke(context, invokeValue) {
+    // The verb "userlike" is sent from the Adaptive Card defined in adaptiveCards/learn.json
+    if (invokeValue.action.verb === "userlike") {
+      this.likeCountObj.likeCount++;
+      const card = cardTools.AdaptiveCards.declare(rawLearnCard).render(this.likeCountObj);
+      await context.updateActivity({
+        type: "message",
+        id: context.activity.replyToId,
+        attachments: [CardFactory.adaptiveCard(card)],
+      });
+      return { statusCode: 200 };
+    }
+  }
+}
+
+module.exports.TeamsBot = TeamsBot;
